0e4f29708f25ff12559e7a9c80d57a73189b3548
Enes Doğan <enesdoganlive@hotmail.com>
2025-05-17 19:06:53 +0300

    Update bash_starter.sh

--- airflows/bash_starter.sh
+++ airflows/bash_starter.sh
@@ -3,11 +3,9 @@
 echo "Airflow veritabanı başlatılıyor..."
 airflow db init
 
-# 2. Airflow web server'ını başlatıyoruz
 echo "Airflow Web UI başlatılıyor..."
-airflow webserver --port 8080 &  # --port 8080, web UI'yi 8080 portunda başlatır.
+airflow webserver --port 8080 &
 
-# 3. Airflow zamanlayıcısını başlatıyoruz (scheduler)
 echo "Airflow zamanlayıcısı başlatılıyor..."
 airflow scheduler &
 
